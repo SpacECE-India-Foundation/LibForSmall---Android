e4adaabf38bca62c64e87b72a25ba4bfe526fe7d
ramashisx <ramashisx@gmail.com>
2022-02-11 12:21:02 +0530

    ui changes from janhavi

--- app/src/main/java/com/spacece/libforsmall/ChefFoodPanel/Chef_PostDish.java
+++ app/src/main/java/com/spacece/libforsmall/ChefFoodPanel/Chef_PostDish.java
@@ -251,7 +251,6 @@ public class Chef_PostDish extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-
         if (mCropimageuri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startCropImageActivity(mCropimageuri);
         } else {
